6346f9d1fdb9ef269ab78454769bf4e46e7e9863
eoijfoiwejfoijwfeoijfeoifwej <43916098+oiejoijweojoweijowej@users.noreply.github.com>
2018-11-04 20:41:59 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,83 @@
+const Discord = require('discord.js');
+const bot1 = new Discord.Client();
+
+
+bot1.on('ready', () => {
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${bot1.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${bot1.guilds.size} " ]`);
+  console.log(`Users! [ " ${bot1.users.size} " ]`);
+  console.log(`channels! [ " ${bot1.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+ 
+
+ bot1.on('message', message => {
+  
+  if (message.content === 'Do your Daily') {
+
+message.channel.send('#daily')
+}
+});
+
+
+ bot1.on('message', message => {
+  
+  if (message.content === 'do rep') {
+
+message.channel.send('#rep <@474573718967025665>')
+}
+});
+
+
+bot1.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('kareem is my dad')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  if(!args) return message.channel.send(`${prefix}say <words>`);
+
+if (command == "say") {
+
+message.channel.send(args.join("  "))
+    message.delete();
+  }
+
+
+
+});
+
+bot1.login("NTA4NzA0MjMxNzU1MjE4OTQ0.DsDOSQ.gZsNbqVlvmLG3jIH1p-2wgJ9GkE")
+
